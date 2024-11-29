f389a92fd100a3a5594853181123e31733276b74
siwakasen <siwakasen@gmail.com>
2024-11-29 14:12:43 +0700

    add entity brands and cars

---
+++ libs/entities/tours/cars.entity.ts
@@ -0,0 +1,66 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  ManyToOne,
+} from 'typeorm';
+import { Brands } from './brands.entity';
+
+@Entity('cars')
+export class Cars {
+  @ApiProperty({
+    description: 'The unique identifier for the car brand',
+    example: '550e8400-e29b-41d4-a716-446655440000',
+  })
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ApiProperty({
+    description: 'The identifier of the car brand',
+    example: '550e8400-e29b-41d4-a716-446655440000',
+  })
+  @ManyToOne(() => Brands, (brand) => brand.id, {
+    onDelete: 'CASCADE',
+  })
+  brand_id: string;
+
+  @ApiProperty({
+    description: 'The name of the car',
+    example: 'Toyota Corolla',
+  })
+  @Column({ type: 'varchar', length: 255 })
+  car_name: string;
+
+  @ApiProperty({
+    description: 'The image of the car',
+    example: 'Toyota Corolla',
+  })
+  @Column({ type: 'varchar', length: 255 })
+  car_image: string;
+
+  @ApiProperty({
+    description: 'The timestamp when the tour package was created',
+    example: '2024-11-18T12:00:00.000Z',
+  })
+  @CreateDateColumn()
+  created_at: Date;
+
+  @ApiProperty({
+    description: 'The timestamp when the tour package was last updated',
+    example: '2024-11-19T12:00:00.000Z',
+  })
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @ApiProperty({
+    description: 'The timestamp when the tour package was soft-deleted',
+    example: '2024-11-20T12:00:00.000Z',
+    nullable: true,
+  })
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
